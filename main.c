b728ddb213bdc2fab8a8bf8b6a61ed991c866ba7
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-04-16 07:46:27 +0900

    add test codes.

--- main.c
+++ main.c
@@ -1,16 +1,34 @@
 #include <string.h>
 #include <jansson.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 
     json_t* json_object;
     json_error_t jerror;
 
-    json_object = json_load_file("./sample.json",0,&jerror);
+    if (argc != 2)
+    {
+        printf("usage: %s <json file>\n", argv[0]);
+        return 1;
+    }
 
+    json_object = json_load_file(argv[1],0,&jerror);
+
+#if 0
     char name[256];
     strcpy(name, json_string_value(json_object_get(json_object, "name")));
     printf("name: %s\n", name);
+#endif
+
+    /* obj is a JSON object */
+    const char *key;
+    json_t *value;
+
+    json_object_foreach(json_object, key, value) {
+        /* block of code that uses key and value */
+        printf("%s:\n", key);
+    }
+    json_decref(json_object);
 }
 
