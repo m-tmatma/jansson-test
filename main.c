bc75697400947086ed3edcdab320b6f1c78a9caa
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-04-16 08:08:35 +0900

    add check

--- main.c
+++ main.c
@@ -32,31 +32,31 @@ int main(int argc, char *argv[])
         {
             printf("Object\n");
         }
-        if (json_is_array(value)  )
+        else if (json_is_array(value)  )
         {
             printf("array\n");
         }
-        if (json_is_string(value) )
+        else if (json_is_string(value) )
         {
             printf("string => '%s'\n", json_string_value(value));
         }
-        if (json_is_integer(value))
+        else if (json_is_integer(value))
         {
             printf("integer => %d\n", json_integer_value(value));
         }
-        if (json_is_real(value)   )
+        else if (json_is_real(value)   )
         {
             printf("real => %f\n", json_real_value(value));
         }
-        if (json_is_number(value) )
+        else if (json_is_number(value) )
         {
             printf("number\n");
         }
-        if (json_is_boolean(value))
+        else if (json_is_boolean(value))
         {
             printf("bool\n");
         }
-        if (json_is_null(value)   )
+        else if (json_is_null(value)   )
         {
             printf("null\n");
         }
