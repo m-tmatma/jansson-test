ce0cbe44e9d481989cad373b38bf8b5737803bb6
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-04-17 07:52:51 +0900

    create empty root object if file parameter is missing.

--- test-overwrite/main.c
+++ test-overwrite/main.c
@@ -6,13 +6,15 @@ int main(int argc, char *argv[])
     json_t* json_root_object;
     json_error_t json_error;
 
-    if (argc != 2)
+    if (argc == 2)
     {
-        printf("usage: %s <json file>\n", argv[0]);
-        return 1;
+        json_root_object = json_load_file(argv[1],0,&json_error);
+    }
+    else
+    {
+        json_root_object = json_object();
     }
 
-    json_root_object = json_load_file(argv[1],0,&json_error);
     json_object_set_new(json_root_object, "test1", json_integer(42));
     json_object_set_new(json_root_object, "test2", json_integer(44));
 
@@ -37,6 +39,11 @@ int main(int argc, char *argv[])
         json_object_set_new(json_root_object, "arraytest", obj);
     }
 
-    printf("%s\n", json_dumps(json_root_object, JSON_INDENT(4)));
+    char * dump = json_dumps(json_root_object, JSON_INDENT(4));
+    if (dump)
+    {
+        printf("%s\n", dump);
+        free(dump);
+    }
     json_decref(json_root_object);
 }
