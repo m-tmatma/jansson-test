906cfdb0fd6cd5cc9e1c608c8040b93ff86b4dba
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-04-16 12:30:34 +0900

    rename varible

--- test-overwrite/main.c
+++ test-overwrite/main.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    json_t* json_object;
-    json_error_t jerror;
+    json_t* json_root_object;
+    json_error_t json_error;
 
     if (argc != 2)
     {
@@ -12,15 +12,15 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    json_object = json_load_file(argv[1],0,&jerror);
-    json_object_set_new(json_object, "test1", json_integer(42));
-    json_object_set_new(json_object, "test2", json_integer(44));
+    json_root_object = json_load_file(argv[1],0,&json_error);
+    json_object_set_new(json_root_object, "test1", json_integer(42));
+    json_object_set_new(json_root_object, "test2", json_integer(44));
 
-    json_t* json_int = json_object_get(json_object, "test1");
+    json_t* json_int = json_object_get(json_root_object, "test1");
     if (json_int)
     {
         json_integer_set(json_int, 10000);
     }
-    printf("%s\n", json_dumps(json_object, JSON_INDENT(4)));
-    json_decref(json_object);
+    printf("%s\n", json_dumps(json_root_object, JSON_INDENT(4)));
+    json_decref(json_root_object);
 }
