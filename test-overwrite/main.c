113871d18c969e80fbb77bbd6e714eef5e926c74
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-04-16 11:58:17 +0900

    add setting existing value

--- test-overwrite/main.c
+++ test-overwrite/main.c
@@ -15,6 +15,12 @@ int main(int argc, char *argv[])
     json_object = json_load_file(argv[1],0,&jerror);
     json_object_set_new(json_object, "test1", json_integer(42));
     json_object_set_new(json_object, "test2", json_integer(44));
+
+    json_t* json_int = json_object_get(json_object, "test1");
+    if (json_int)
+    {
+        json_integer_set(json_int, 10000);
+    }
     printf("%s\n", json_dumps(json_object, JSON_INDENT(4)));
     json_decref(json_object);
 }
