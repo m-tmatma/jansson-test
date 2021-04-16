1e90ddd08cc06ec5c20a4bf1457f5804c8845d51
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-04-16 11:49:31 +0900

    add value

--- test-overwrite/main.c
+++ test-overwrite/main.c
@@ -13,5 +13,8 @@ int main(int argc, char *argv[])
     }
 
     json_object = json_load_file(argv[1],0,&jerror);
+    json_object_set_new(json_object, "test1", json_integer(42));
+    json_object_set_new(json_object, "test2", json_integer(44));
+    printf("%s\n", json_dumps(json_object, JSON_INDENT(4)));
     json_decref(json_object);
 }
